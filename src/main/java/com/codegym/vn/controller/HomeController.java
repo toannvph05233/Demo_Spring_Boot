ec3851ffccc0f95aad1b52e6eeb6596ab1804da6
toannvph05233 <johntoan98@gmail.com>
2022-01-21 16:21:52 +0700

    upp webservice

--- src/main/java/com/codegym/vn/controller/HomeController.java
+++ src/main/java/com/codegym/vn/controller/HomeController.java
@@ -1,20 +1,42 @@
 package com.codegym.vn.controller;
 
+import com.codegym.vn.model.Product;
 import com.codegym.vn.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
 public class HomeController {
     @Autowired
     IProductService productService;
 
-    @GetMapping("/home")
-    public ModelAndView show(){
-       ModelAndView modelAndView = new ModelAndView("/product/show");
-       modelAndView.addObject("products", productService.findAll());
-       return modelAndView;
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> show() {
+        return new ResponseEntity<>(productService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        productService.save(product);
+        return new ResponseEntity<>(product,HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void delete(@PathVariable long id) {
+        productService.delete(id);
+    }
+
+    @PutMapping("/products/{id}")
+    public void update(@RequestBody Product product) {
+        productService.save(product);
     }
 }
